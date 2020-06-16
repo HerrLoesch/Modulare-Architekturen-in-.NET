e686006833943a612b0b4ccef4d7ff8f3fc96f6a
Hendrik Lösch <loeschhendrik@googlemail.com>
2020-06-16 21:54:21 +0200

    Added patient module and measurement module as well as their views

---
+++ Measurement.Module/Class1.cs
@@ -0,0 +1,26 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Measurement.UI;
+
+namespace Measurement.Module
+{
+    public class MeasurementModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            var regionManager = containerProvider.Resolve<IRegionManager>();
+            regionManager.RegisterViewWithRegion("MainRegion", typeof(MeasurementView));
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+    }
+}
--- Medizinsoftware/App.xaml.cs
+++ Medizinsoftware/App.xaml.cs
@@ -5,13 +5,38 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Measurement.Module;
+using Patient.Module;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Unity;
 
 namespace Medizinsoftware
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : PrismApplication
     {
+        protected override Window CreateShell()
+        {
+            return new MainWindow();
+        }
+
+        protected override IModuleCatalog CreateModuleCatalog()
+        {
+            return new ModuleCatalog();
+        }
+
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+            moduleCatalog.AddModule(typeof(PatientModule));
+            moduleCatalog.AddModule(typeof(MeasurementModule));
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
     }
 }
