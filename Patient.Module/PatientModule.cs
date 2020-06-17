34bc424a6a4d1291271de4ab27cda1134d39fce4
Hendrik Lösch <loeschhendrik@googlemail.com>
2020-06-17 09:08:41 +0200

    Added ViewModel and navigation for Patient module.

--- Patient.Module/PatientModule.cs
+++ Patient.Module/PatientModule.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Infrastructure.Interfaces;
 using Patient.UI;
+using Patient.UI.Views;
 
 namespace Patient.Module
 {
@@ -20,6 +21,9 @@ namespace Patient.Module
 
             var navigationRegistry = containerProvider.Resolve<INavigationRegistry>();
             navigationRegistry.RegisterNavigationStep(typeof(PatientModule), typeof(PatientView));
+
+            var navigationService = containerProvider.Resolve<INavigationService>();
+            navigationService.NavigateTo(typeof(PatientModule));
         }
 
         public void RegisterTypes(IContainerRegistry containerRegistry)
---
+++ Patient.UI/ViewModels/PatientViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Infrastructure.Interfaces;
+using Prism.Commands;
+
+namespace Patient.UI.ViewModels
+{
+    public class PatientViewModel
+    {
+        private INavigationService navigationService;
+
+        public DelegateCommand NavigateNextCommand { private set; get; }
+
+        public PatientViewModel(INavigationService navigationService)
+        {
+            this.navigationService = navigationService;
+            NavigateNextCommand = new DelegateCommand(GoToNext);
+        }
+
+        private void GoToNext()
+        {
+            navigationService.Next();
+        }
+    }
+}
